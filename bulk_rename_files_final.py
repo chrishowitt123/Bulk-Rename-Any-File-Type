097806a98b19e42277f731109937bebf01d95b37
chrishowitt123 <62956122+chrishowitt123@users.noreply.github.com>
2020-06-19 00:59:19 +0100

    Update bulk_rename_files_final.py

--- bulk_rename_files_final.py
+++ bulk_rename_files_final.py
@@ -8,40 +8,29 @@ import numpy
 from datetime import datetime
 
 
-path = input('Please paste the path of the files you wish to rename and press enter: \n')
-
+path = input('\n Please paste the path to the the files you wish to rename and press enter: ')
 os.chdir(path)
-
 files = [f for f in listdir(path) if isfile(join(path, f))]
 
-
 df = pd.DataFrame(files)
+df['old_file_names'] = pd.DataFrame(files)
+del df[0]
 
-df['Files'] = pd.DataFrame(files)
-
-df['Files'] 
-
-file_names = list(df['Files'])
+file_names = list(df['old_file_names'])
 file_names = [ file for file in file_names if ".ipynb" not in file ]
 files = file_names
-df['Files'] = pd.DataFrame(files)
+df['old_file_names'] = pd.DataFrame(files)
 
-df['New File Names'] = df['Files'].str.split('.').str[0].str.split('(').str[0]
-
+df['new_file_names'] = df['old_file_names'].str.split('.').str[0].str.split('(').str[0]
+exe = df['old_file_names'].str.split('.').str[-1].str.split('(').str[0]
 date_today = datetime.today().strftime('%Y-%m-%d')
-date_today
 
-document_type = input('What type of documents are these? (followed by press enter) \n').title()
+document_type = input('\n Enter your document identifier: ').title()
 
-df['New File Names'] = (date_today + '_' + df['New File Names'].str.title() + '_' + document_type).str.title()
+df['new_file_names'] = (date_today + '_' + df['new_file_names'].str.title() + '_' + document_type).str.title()
 
-df.columns
 
-df.columns = (['old', 'Files', 'New File Names'])
-
-del df['old']
-
-df['New File Names'] = df['New File Names'] + '.txt'
+df['new_file_names'] = df['new_file_names'] + '.' + exe
 
 df
 
@@ -52,7 +41,17 @@ files = [f for f in path1.iterdir() if f.is_file()]
 
 for file in files:
     try:
-        prefix = df.loc[df['Files'] == file.name, 'New File Names'].values[0]
+        prefix = df.loc[df['old_file_names'] == file.name, 'new_file_names'].values[0]
         file.rename(f'{prefix}')
     except IndexError:
         pass
+
+
+old = list(df['old_file_names'])
+new = list(df['new_file_names'])
+
+for o, n in zip(old, new):
+    print('\n ' + o + '    ---------->    ' + n)
+
+
+
